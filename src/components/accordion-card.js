442e7db133e77f01cced158b601451faa1bc99a0
Tara <taratrody@gmail.com>
2021-10-02 23:58:13 -0400

    style accordion background

--- src/components/accordion-card.js
+++ src/components/accordion-card.js
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components/macro";
 
 const Accordion = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <CardBackground></CardBackground>
+    </Container>
+  );
 };
 
 export default Accordion;
@@ -11,4 +15,16 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(#b068e9, #6463e7);
+`;
+
+const CardBackground = styled.div`
+  width: 87%;
+  height: 70%;
+  background-color: #fff;
+  border-radius: 23px;
+
+  @media (min-width: 768px) {
+    width: 63%;
+    height: 66%;
+  }
 `;
