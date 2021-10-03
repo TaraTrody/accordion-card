3e0e085f122e106bcdd33b02b7ba4bde8e5b2976
Tara <taratrody@gmail.com>
2021-10-03 00:37:46 -0400

    replaced grid settings with flexbox

--- src/components/accordion-card.js
+++ src/components/accordion-card.js
@@ -15,22 +15,17 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(#b068e9, #6463e7);
-  display: grid;
-  grid-template-colums: 1;
-  grid-template-rows: 1;
-  justify-items: center;
+  display: flex;
+  justify-content: center;
   align-items: center;
 `;
 
 const CardBackground = styled.div`
-  grid-column: 1;
-  grid-row: 1;
   width: 87%;
   height: 70%;
   background-color: #fff;
   border-radius: 23px;
-
-  @media (min-width: 768px) {
+  display: grid @media (min-width: 768px) {
     width: 63%;
     height: 66%;
   }
