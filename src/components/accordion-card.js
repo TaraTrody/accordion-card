46b14481b91c5b3c186fbe5bcaf9ee4d78999ab3
Tara <taratrody@gmail.com>
2021-10-02 23:32:20 -0400

    style container with gradient

--- src/components/accordion-card.js
+++ src/components/accordion-card.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Accordion = () => {
+  return <Container></Container>;
+};
 
-}
+export default Accordion;
 
-export default Accordion
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(#b068e9, #6463e7);
+`;
