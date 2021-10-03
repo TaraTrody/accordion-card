43a5239dbe5b257dc7ef5a888af43eeeaa41a53a
Tara <taratrody@gmail.com>
2021-10-02 22:53:23 -0400

    create accordion component and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Accordion from "./components/index.js";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Accordion from "./accordion-card.js";
+
+export default Accordion;
