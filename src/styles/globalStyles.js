f87abf7d43433a37d90b2f9375d98ce37632ac35
Tara <taratrody@gmail.com>
2021-10-04 23:25:22 -0400

    remove import stmts for fonts and reference in fontface

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,37 +1,48 @@
 import { createGlobalStyle } from "styled-components";
-import KumbhBoldWoff from "public/fonts/kumbh-sans-v6-latin-700.woff";
-import KumbhBoldWoff2 from "public/fonts/kumbh-sans-v6-latin-700.woff2";
-import KumbhRegWoff from "public/fonts/kumbh-sans-v6-latin-regular.woff";
-import KumbhRegWoff2 from "public/fonts/kumbh-sans-v6-latin-regular.woff2";
 
 const GlobalStyles = createGlobalStyle`
 
-@font-face {
-  font-family: 'Kumbh Sans';
-  font-style: normal;
-  font-weight: 400;
-  src: url(${KumbhRegWoff2}) format('woff2'),
-       url(${KumbhRegWoff}) format('woff'), 
-}
+  @font-face {
+    font-family: 'Kumbh Sans';
+    font-style: normal;
+    font-weight: 400;
+    local(");
+    src: url("/kumbh-sans-v6-latin-regular.woff2") format('woff2'),
+        url("/kumbh-sans-v6-latin-regular.woff") format('woff'), 
+  }
 
-@font-face {
-  font-family: 'Kumbh Sans';
-  font-style: normal;
-  font-weight: 700;
-  src: url(${KumbhBoldWoff2}) format('woff2'),
-       url(${KumbhBoldWoff}) format('woff'),
-}
+  @font-face {
+    font-family: 'Kumbh Sans';
+    font-style: normal;
+    font-weight: 700  ;
+    src: url("/kumbh-sans-v6-latin-700.woff2") format('woff2'),
+        url("/kumbh-sans-v6-latin-700.woff") format('woff'),
+  }
 
-* {
-  box-sizing: border-box;
-}
+  *, 
+  *:before, 
+  *:after {
+      box-sizing: border-box;
+  }
 
-body {
+  * {
     margin: 0;
-    font-family: "Kumbh Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-}
+    padding: 0;
+  }
+
+  html,
+  body {
+      width: 100%;
+      height: 100%;
+      padding: 0;
+      margin: 0;
+  }
+
+  body {
+      font-family: "Kumbh Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+  }
 `;
 
 export default GlobalStyles;
